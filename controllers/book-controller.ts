d4c6f4e89955dc53fbdc476e7da9bd6a418d6b01
Henry Rothenberg <hsr205@aol.com>
2025-02-25 15:03:51 -0500

    Complete controller

--- controllers/book-controller.ts
+++ controllers/book-controller.ts
@@ -1,35 +1,37 @@
 import {RequestHandler} from "express";
 import {isValidObjectId} from "mongoose";
 import createHttpError from "http-errors";
-import BookSchema from "../models/book";
 import BookInstanceSchema from "../models/bookinstance";
 
 export const getBookDetails: RequestHandler = async (request, response, next) => {
 
 
-    const bookId = request.query.id;
+    const bookId = request.params.id;
 
-    console.log(`bookId = ${bookId}`);
 
     try {
         if (!isValidObjectId(bookId)) {
             throw createHttpError(400, `Invalid question id provided`);
         }
-        // // TODO: Use the following as basic queries to see the document structure for both BookSchema and BookInstanceSchema
-        // const allBooks = await BookSchema.find({}, "title author").populate("author");
-        // const allBookInstances = await BookInstanceSchema.find({}, "imprint status").populate("book");
 
-        const allBooks = await BookSchema.findById(bookId, "title author").populate('books');
+        const bookDetailsObj = await BookInstanceSchema.find({book: bookId}, "imprint status")
+            .populate({
+                path: "book",
+                select: "title author",
+                populate: {
+                    path: "author",
+                    select: "first_name family_name"
+                }
+            });
 
-        // TODO: The following query will need to change to find the specific book ID with the BookInstanceSchema
-        //       This should create a join making it possible to get the fields "imprint" and "status" from the BookInstanceSchema
-        const allBookInstances = await BookInstanceSchema.find({}, "imprint status").populate("book");
 
-        // if (!question) {
-        //     throw createHttpError(404, `Question not found`);
-        // }
-        //
-        // response.status(200).json(question);
+        if (!bookDetailsObj) {
+            throw createHttpError(404, `Question not found`);
+        }
+
+
+        response.status(200).json(bookDetailsObj);
+
     } catch (error) {
         next(error);
     }
